d65594fb8548d28ed3ba3fcd3e235252f9670156
liuyue <liuyue@tsa.cn>
2019-03-14 17:42:03 +0800

    swiper练习

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,11 +1,50 @@
 <template>
   <div>
-    <div>
-      <img src= "../../assets/logo.png" v-on:click="imgUrl" />
+    <mt-header title="首页">
+      <mt-button slot="right"></mt-button>
+    </mt-header>
+    <div class="page-swipe">
+      <mt-swipe :auto="4000" class="swipe">
+        <mt-swipe-item class="slide1">1</mt-swipe-item>
+        <mt-swipe-item class="slide2">2</mt-swipe-item>
+        <mt-swipe-item class="slide3">3</mt-swipe-item>
+      </mt-swipe>
     </div>
 
   </div>
 </template>
+<style>
+  .page-swipe {
+    text-align: center;
+    color: #666;
+    margin-bottom: 5px;
+  }
+  .swipe {
+    height: 200px;
+    color: #fff;
+    font-size: 30px;
+    text-align: center;
+    margin-bottom: 20px;
+  }
+
+  .slide1 {
+    background-color: #0089dc;
+    color: #fff;
+    line-height: 200px;
+  }
+
+  .slide2 {
+    background-color: #ffd705;
+    color: #000;
+    line-height: 200px;
+  }
+
+  .slide3 {
+    background-color: #ff2d4b;
+    color: #fff;
+    line-height: 200px;
+  }
+</style>
 <script>
 export default {
   methods: {
