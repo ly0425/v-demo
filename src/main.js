e3454ec358182b60b63123528a86bf1fd551a55d
liuyue <liuyue@tsa.cn>
2019-04-03 10:15:28 +0800

    vant ui组件库引入

--- src/main.js
+++ src/main.js
@@ -2,16 +2,19 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import Mint from 'mint-ui'
+import Vant from 'vant'
 import axios from 'axios'
-import 'mint-ui/lib/style.css'
 import App from './App'
 import router from './router'
+import 'mint-ui/lib/style.css'
+import 'vant/lib/index.css'
 import './assets/css/iconfont/iconfont.css'
 import './assets/css/common.css'
 
 Vue.config.productionTip = false
 Vue.prototype.$axios = axios
 Vue.use(Mint)
+Vue.use(Vant)
 
 /* eslint-disable no-new */
 new Vue({
