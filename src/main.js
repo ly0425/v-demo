e5705f78a41b26dd9dbf647a0d1d7a6f4ad33097
liuyue <liuyue@tsa.cn>
2019-04-04 17:38:22 +0800

    vuex增加

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import Mint from 'mint-ui'
 import Vant from 'vant'
 import axios from 'axios'
+import Vuex from 'vuex'
 import App from './App'
 import router from './router'
 import 'mint-ui/lib/style.css'
@@ -15,11 +16,50 @@ Vue.config.productionTip = false
 Vue.prototype.$axios = axios
 Vue.use(Mint)
 Vue.use(Vant)
+Vue.use(Vuex)
 
+const store = new Vuex.Store({
+  state: {
+    products: [
+      {name: '鼠标', price: 20},
+      {name: '键盘', price: 40},
+      {name: '耳机', price: 60},
+      {name: '显示屏', price: 80}
+    ]
+  },
+  getters: { // 添加getters
+    saleProducts: (state) => {
+      let saleProducts = state.products.map(product => {
+        return {
+          name: product.name,
+          price: product.price / 2
+        }
+      })
+      return saleProducts
+    }
+  },
+  mutations: { // 添加mutations
+    minusPrice (state, payload) {
+      let newPrice = state.products.forEach(product => {
+        product.price -= payload
+      })
+      return newPrice
+    }
+  },
+  actions: { // 添加actions
+    minusPriceAsync (context, payload) {
+      setTimeout(() => {
+        context.commit('minusPrice', payload) // context提交
+      }, 2000)
+    }
+  }
+})
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
+  render: h => h(App),
   components: { App },
   template: '<App/>',
   mounted () {
