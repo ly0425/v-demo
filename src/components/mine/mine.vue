41f651464ff5fc56e71dec86ef9aeb3e69024529
liuyue <liuyue@tsa.cn>
2019-04-04 17:37:42 +0800

    vuex支持

--- src/components/mine/mine.vue
+++ src/components/mine/mine.vue
@@ -3,11 +3,20 @@
     <mt-header title="我的">
       <mt-button slot="right"></mt-button>
     </mt-header>
+    <product-list-one v-bind:products="products"></product-list-one>
+    <product-list-two v-bind:products="products"></product-list-two>
   </div>
 </template>
 
 <script>
+import ProductListOne from '../store/ProductListOne.vue'
+import ProductListTwo from '../store/ProductListTwo.vue'
 export default {
+  name: 'mine',
+  components: {
+    'product-list-one': ProductListOne,
+    'product-list-two': ProductListTwo
+  }
 }
 </script>
 
