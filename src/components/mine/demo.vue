20d331763edb7820f109d449b4c873cf0614c4e1
liuyue <liuyue@tsa.cn>
2019-04-10 17:06:08 +0800

    vuex

--- src/components/mine/demo.vue
+++ src/components/mine/demo.vue
@@ -1,13 +1,21 @@
 <template>
-    $END$
+  <div>
+    <mt-header title="小练习">
+      <mt-button v-on:click="$router.back()" slot="left" icon="back"></mt-button>
+    </mt-header>
+    <product-list-one v-bind:products="products"></product-list-one>
+    <product-list-two v-bind:products="products"></product-list-two>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "demo"
-    }
+import ProductListOne from '../store/ProductListOne.vue'
+import ProductListTwo from '../store/ProductListTwo.vue'
+export default {
+  name: 'demo',
+  components: {
+    'product-list-one': ProductListOne,
+    'product-list-two': ProductListTwo
+  }
+}
 </script>
-
-<style scoped>
-
-</style>
