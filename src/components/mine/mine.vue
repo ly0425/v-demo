ae3a45abbe07b77a1a87056fc7cafe9131622bbf
liuyue <liuyue@tsa.cn>
2019-04-10 17:06:57 +0800

    子文件路由跳转

--- src/components/mine/mine.vue
+++ src/components/mine/mine.vue
@@ -3,23 +3,16 @@
     <mt-header title="我的">
       <mt-button slot="right"></mt-button>
     </mt-header>
-    <product-list-one v-bind:products="products"></product-list-one>
-    <product-list-two v-bind:products="products"></product-list-two>
+    <div>
+      <router-link to="/mine/demo">
+        <mt-button>demo测试</mt-button>
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
-import ProductListOne from '../store/ProductListOne.vue'
-import ProductListTwo from '../store/ProductListTwo.vue'
 export default {
-  name: 'mine',
-  components: {
-    'product-list-one': ProductListOne,
-    'product-list-two': ProductListTwo
-  }
+  name: 'mine'
 }
 </script>
-
-<style scoped>
-
-</style>
